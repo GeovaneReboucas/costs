28932581e85c57e30aa9b9092331281c8d86e51e
GeovaneReboucas <geovane.reboucas2002@gmail.com>
2022-01-11 01:43:26 -0300

    exclusion from services

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -111,7 +111,28 @@ function Project() {
 
     }
 
-    function removeService(){
+    function removeService(id, cost){
+        const servicesUpdated = project.services.filter((service) => service.id !== id);
+
+        const projectUpdated = project;
+
+        projectUpdated.services =  servicesUpdated;
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(projectUpdated)
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated);
+            setServices(servicesUpdated);
+            setMessage('Serviço removido com sucesso!');
+            setType('success');
+        })
+        .catch((err) => console.log(err));
 
     }
 
